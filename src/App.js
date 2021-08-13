f1ad1b8fb25cbcbb5cfbb534646fd23b79c1f5c4
stefanbauer <alternative2k@googlemail.com>
2021-08-13 11:11:33 +0200

    added LinkToRepo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Header from "./components/Header";
 import UserInfo from "./components/UserInfo";
 import InputForm from "./components/InputForm";
+import LinkToRepo from "./components/LinkToRepo";
 
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
               <Header />
               <UserInfo />
               <InputForm />
+              <LinkToRepo />
           </Route>
         </Switch>
       </Router>
---
+++ src/components/LinkToRepo.js
@@ -0,0 +1,9 @@
+import {Link} from "react-router-dom";
+
+export default function LinkToRepo() {
+    return(
+        <section>
+            <Link to="/"> test</Link>
+        </section>
+    );
+}
